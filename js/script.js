c5a7f3de21cda588a7929483e2ea3e58588cb0e9
DamirFrontMan <75247241+DamirFrontMan@users.noreply.github.com>
2020-11-30 17:42:26 +0600

    patch 1.1

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function ready() {
 	
 	}
 
-	function skrF (){
+	function skrF(){
 		var firstSkrillBlok = document.querySelector(".mySkills");
 		var secSkrillBlok = document.querySelector(".secS");
 		var secTwoBlk = document.querySelector(".skilS_anim");
@@ -26,6 +26,47 @@ function ready() {
 			secTwoBlk.classList.add("active");
 		}
 	}
+
+	function blackS(){
+		var btn = document.querySelector(".blackStyle");
+		var obj = document.querySelector(".bgContent");
+		var tit = document.querySelectorAll(".titT");
+		var ico = document.querySelectorAll('.ico');
+
+		if(obj.classList.contains('active')){
+			for (var i = 0.; i < tit.length; i++) {
+    			tit[i].classList.remove('active');
+    		}
+
+    		for (var i = 0; i < ico.length; i++) {
+    			ico[i].classList.remove('active');
+    		}
+
+    		btn.title = 'Темная тема';
+    		btn.classList.remove('active');
+        	obj.classList.remove('active');
+        	document.getElementById('spn').style.background = 'black';
+        	document.body.style.color = 'black';
+    		document.getElementById("blC").style.color = 'black';
+    	}else {
+    		for (var i = 0.; i < tit.length; i++) {
+    			tit[i].classList.add('active');
+    		}
+
+    		for (var i = 0; i < ico.length; i++) {
+    			ico[i].classList.add('active');
+    		}
+
+    		btn.title = 'Светлая тема';
+    		btn.classList.add('active');
+    		document.getElementById('spn').style.background = 'white';
+    		document.body.style.color = 'white';
+    		document.getElementById("blC").style.color = 'white';
+        	obj.classList.add('active');
+    	}
+	}
+
+	document.querySelector(".blackStyle").addEventListener("click", blackS);
 	window.addEventListener('scroll', skrF);
 	window.onload = animSld;
 }
